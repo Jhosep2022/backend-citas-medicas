19933afa251478be579f69a093d890c5d5d27f0a
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2024-11-09 10:25:53 -0400

    creacion del proyecto en backend citas medicas

---
+++ src/main/kotlin/com/example/backendcitamedica/BackendcitamedicaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backendcitamedica
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendcitamedicaApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendcitamedicaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/backendcitamedica/BackendcitamedicaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backendcitamedica
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendcitamedicaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
